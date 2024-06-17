baf7181334f1328a15b8c6d69ef4a9759cb8948e
balijepallianil <balijepallianilg@gmail.com>
2024-06-17 19:07:08 +0530

    modified groovy

--- src/com/i27academy/builds/Calculator.groovy
+++ src/com/i27academy/builds/Calculator.groovy
@@ -1,1 +1,10 @@
 package com.i27academy.builds
+
+class calculator {
+    def jenkins
+    calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+
+
+}
---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,13 @@
+package com.i27academy.builds
+
+class Docker {
+    def (jenkins)
+    docker(jenkins) {
+            this.jenkins = jenkins
+    }
+
+    def add(irstNumber, secondNumber) {
+     return firstNumber+secondNumber
+    }
+
+}
