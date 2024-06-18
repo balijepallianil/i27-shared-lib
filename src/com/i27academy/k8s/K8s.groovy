e465c954131c38dc2587327660a9c49bef1fe28c
balijepallianil <balijepallianilg@gmail.com>
2024-06-18 12:59:17 +0530

    m19

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -3,26 +3,6 @@ package com.i27academy.k8s
 class K8s {
     def jenkins
     K8s(jenkins) {
-            this.jenkins = jenkins
+        this.jenkins = jenkins
     }
-    def buildApp() {
-        return {
-            echo "Bulding ${env.APPLICATION_NAME} application"
-            sh 'mvn clean package -DskipTests=true'
-
-        }
-    }
-    def dockerBuildandPush() {
-        return {
-            echo "Starting Docker build stage"
-            sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd/"
-            echo "**************************** Building Docker Image ****************************"
-            sh "docker build --force-rm --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
-            echo "********Docker login******"
-            sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
-            echo "********Docker Push******"
-            sh "docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-        }
-    }
-
 }
--- vars/K8spipeline.groovy
+++ vars/K8spipeline.groovy
@@ -62,3 +62,23 @@ def call(Map pipelineParams) {
     }
 }
 }
+
+def buildApp() {
+        return {
+            echo "Bulding ${env.APPLICATION_NAME} application"
+            sh 'mvn clean package -DskipTests=true'
+
+        }
+    }
+def dockerBuildandPush() {
+        return {
+            echo "Starting Docker build stage"
+            sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd/"
+            echo "**************************** Building Docker Image ****************************"
+            sh "docker build --force-rm --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
+            echo "********Docker login******"
+            sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+            echo "********Docker Push******"
+            sh "docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+        }
+    }
